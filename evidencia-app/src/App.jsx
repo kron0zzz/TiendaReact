8d272b4d6601b4c47f9917bbaaa81f3b3fbe97d5
kron0zzz <alanandrey234@gmail.com>
2025-09-04 08:56:18 -0500

    este es el primer commit, aún me falta actualizar

---
+++ evidencia-app/src/App.jsx
@@ -0,0 +1,19 @@
+
+import Header from './features/shared/components/header'
+import Slider from './features/shared/components/slider'
+import Productos from './features/dashboard/components/productos'
+import Footer from './features/shared/components/footer'
+
+function App(){
+  return (
+    <>
+    <Header/>
+    <Slider/>
+    <Productos/>
+    <Footer/>
+
+    </>
+  )
+}
+
+export default App
---
+++ evidencia-app/src/features/shared/components/slider.jsx
@@ -0,0 +1,22 @@
+import '../styles/slider.css'
+function Slider() {
+
+
+  return (
+    <div className='slider'>
+      <div className="imagen">
+            <div className="texto">
+                <h2>Discover the Latest in Home Design</h2>   
+                <p>Explora nuestra colección seleccionada de muebles y decoración para realzar tu espacio vital. Desde lo esencial minimalista hasta piezas llamativas.</p>
+            </div>
+      </div>
+    </div>
+  
+  )
+}
+
+
+
+export default Slider
+
+
